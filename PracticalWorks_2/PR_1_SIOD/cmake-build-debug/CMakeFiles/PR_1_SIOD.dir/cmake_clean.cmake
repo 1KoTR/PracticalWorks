f8a8af974b4c36874c90ff3b0ed41c554b3b7dec
KoTR <denis.kotr@gmail.com>
2022-03-11 19:22:55 +0300

    PR_1_SIOD created

---
+++ PracticalWorks_2/PR_1_SIOD/cmake-build-debug/CMakeFiles/PR_1_SIOD.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PR_1_SIOD.dir/main.c.obj"
+  "PR_1_SIOD.exe"
+  "PR_1_SIOD.exe.manifest"
+  "PR_1_SIOD.pdb"
+  "libPR_1_SIOD.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PR_1_SIOD.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
